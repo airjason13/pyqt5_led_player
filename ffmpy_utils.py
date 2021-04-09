517a9838ebaf5fb885ae218fa9c9bcbba35dac66
airjason13 <airjason13@gmail.com>
2021-04-09 18:16:26 +0800

    fix thumbnail gen function

--- ffmpy_utils.py
+++ ffmpy_utils.py
@@ -9,7 +9,7 @@ def get_video_duration(video_path):
     frame_count = video.get(cv2.CAP_PROP_FRAME_COUNT)
     fps = video.get(cv2.CAP_PROP_FPS)
     duration_ms = frame_count/fps
-    #print(duration_ms)
+    print(duration_ms)
     #print(frame_count)
     return duration_ms
 
@@ -18,10 +18,17 @@ def get_thumbnail_from_video(video_path):
     thumbnail_path = video_path.replace(".mp4", ".jpg")
     duration_ms = get_video_duration(video_path)
     print(type(duration_ms))
+    duration_ms -= 1
+    str_ms = str(duration_ms)
+    print("str_ms :", str_ms)
     ff = ffmpy.FFmpeg(
         inputs={video_path: None},
-        outputs={thumbnail_path: ['-ss', '00:00:10.000', '-vframes', '1']}
+        outputs={thumbnail_path: ['-ss', str_ms, '-vframes', '1']}
     )
+    """ff = ffmpy.FFmpeg(
+        inputs={video_path: None},
+        outputs={thumbnail_path: ['-ss', '00:00:10.000', '-vframes', '1']}
+    )"""
     
     ff.run()
     return thumbnail_path
